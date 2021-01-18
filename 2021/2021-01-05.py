5485205ad128188f94aa46e7735f3053d2e0957a
Jessica Desotell <jyspaun@mtu.edu>
2021-01-18 09:05:37 -0500

    Add files via upload

---
+++ 2021/2021-01-05.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+transit_cost = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-05/transit_cost.csv') 
+
+print(transit_cost.columns)
